f94211521721640a44411a1fa3edfe5b4462bce8
William Thompson <wmthmps1@gmail.com>
2024-01-19 12:52:31 -0600

    Created Update Product Method

--- Testing/Models/ProductRepository.cs
+++ Testing/Models/ProductRepository.cs
@@ -23,12 +23,17 @@ namespace Testing.Models
         {
             return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", new { id = id });
         }
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+                new { name = product.Name, price = product.Price, id = product.ProductID });
+
+        }
     }
 }
 
 /*
- * public Product GetProduct(int id)
-{
-    return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", new { id = id });
-}
+ _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+            new { name = product.Name, price = product.Price, id = product.ProductID });
  */
